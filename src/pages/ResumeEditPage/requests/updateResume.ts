7ca6ddd412a141d2f90f6199e46662faba8ed760
Ibragim Khaibullin <khaibullin.ibr@gmail.com>
2023-12-07 11:07:23 +0600

    Fixed requests and one translation

--- src/pages/ResumeEditPage/requests/updateResume.ts
+++ src/pages/ResumeEditPage/requests/updateResume.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { SERVER_URL } from 'shared/api/api';
 
 export const updateResume = async (resumeId: number, updatedResume: any, authToken: string | undefined) => {
     try {
-        const response = await axios.put(`http://localhost:3000/resume/${resumeId}`, {
+        const response = await axios.put(`${SERVER_URL}/resume/${resumeId}`, {
             resume: updatedResume,
         }, {
             headers: {
--- src/pages/ResumePage/requests/getResume.ts
+++ src/pages/ResumePage/requests/getResume.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { SERVER_URL } from 'shared/api/api';
 
 export const fetchResumeData = async (resumeId: number) => {
     try {
-        const response = await axios.get(`http://localhost:3000/resume/${resumeId}`);
+        const response = await axios.get(`${SERVER_URL}/resume/${resumeId}`);
         return response.data;
     } catch (error) {
         console.error('Error fetching resume data:', error);
