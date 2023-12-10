3fccb04fca2eec9fea775daf08476d9dee7a1228
Ibragim Khaibullin <khaibullin.ibr@gmail.com>
2023-12-10 14:05:20 +0600

    Changed API url

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { USER_LOCAL_STORAGE_KEY } from 'shared/consts/localstorage';
 
-export const SERVER_URL = 'https://insangelhub-89ed907960d2.herokuapp.com';
+export const SERVER_URL = 'https://nestinsangelhub-6f3fe1e7f4ab.herokuapp.com';
 
 export const $api = axios.create({
     baseURL: SERVER_URL,
